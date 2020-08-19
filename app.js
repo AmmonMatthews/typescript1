b7ac5ad5115e5c33867aa8d6da9463f390020b00
AmmonMatthews <ammonmatthews@gmail.com>
2020-08-19 12:57:51 -0600

    array type

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-var person = { name: "ammon", age: 29 };
-console.log(person);
+// const person: {
+//     name: string;
+//     age: number;
+// } =
+var person = {
+    name: 'ammon',
+    age: 29,
+    hobbies: ['Sports', 'Cooking']
+};
+console.log(person.name);
+var favoirteActivities;
+favoirteActivities = ['Sports'];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -2,6 +2,19 @@
 //     name: string;
 //     age: number;
 // } =
-   const person = { name: 'ammon', age: 29 };
+const person = {
+    name: 'ammon',
+    age: 29,
+    hobbies: ['Sports','Cooking']
+};
 
 console.log(person.name)
+
+
+let favoirteActivities: string[];
+favoirteActivities = ['Sports']
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) This is an error because type string doesn't support. 
+}
