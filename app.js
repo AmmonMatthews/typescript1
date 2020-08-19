59f4db329d06e6abe9623073344bf7d5d936b26b
AmmonMatthews <ammonmatthews@gmail.com>
2020-08-19 11:30:01 -0600

    basic function setup with typescript

---
+++ app.js
@@ -0,0 +1,7 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number = 5;
+var number1 = 2.8;
+var result = add(number, number1);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,11 @@
+function add(n1:number, n2:number) {
+    return n1 + n2
+
+}
+
+const number = "5"
+const number1 = 2.8
+
+const result = add(number, number1);
+
+console.log(result)
