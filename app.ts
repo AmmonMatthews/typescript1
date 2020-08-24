f9c398be6b1b5fe67d9023bce6b9457b3e31d33a
AmmonMatthews <ammonmatthews@gmail.com>
2020-08-24 10:23:59 -0600

    commiting anything not up on github

--- app.ts
+++ app.ts
@@ -2,11 +2,24 @@
 //     name: string;
 //     age: number;
 // } =
-const person = {
-    name: 'ammon',
-    age: 29,
-    hobbies: ['Sports','Cooking']
-};
+
+//You can create an interface object seperate or utilize it in line
+// or you can let typescript infere the types being created. This works for everything except for tuples
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string] // Doing this creates an array of only two numbers are allowed with minimal exceptition
+// } ={
+//     name: 'ammon',
+//     age: 29,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'author']  //tuple 
+// };
+//PUsh is an exception and will always add on to the tuple 
+// person.role.push('admin') 
+//person.role[1] = 8 would not work because it is not the correct type
+// person.role = [9, 'admin' , ' author,m'] would not work because it exceeds the length
 
 console.log(person.name)
 
